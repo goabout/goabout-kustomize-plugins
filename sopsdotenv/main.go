26e00bf184b61a68a3c7b4aade8354277fa30a65
Joost Cassee <joost@cassee.net>
2019-04-14 22:34:21 +0200

    Use loader to read files to mirror builtin plugins

--- sopsdotenv/main.go
+++ sopsdotenv/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"go.mozilla.org/sops/decrypt"
 	"sigs.k8s.io/kustomize/k8sdeps/kv"
+	"sigs.k8s.io/kustomize/pkg/fs"
+	"sigs.k8s.io/kustomize/pkg/loader"
 )
 
 type sopsDotEnv struct{}
@@ -11,9 +12,19 @@ type sopsDotEnv struct{}
 var KVSource sopsDotEnv
 
 func (p *sopsDotEnv) Get(root string, args []string) (map[string]string, error) {
+	ldr, err := loader.NewLoader(root, fs.MakeRealFS())
+	if err != nil {
+		return nil, err
+	}
+
 	allKvs := make(map[string]string)
-	for _, arg := range args {
-		data, err := decrypt.File(fmt.Sprintf("%s/%s", root, arg), "dotenv")
+	for _, fPath := range args {
+		content, err := ldr.Load(fPath)
+		if err != nil {
+			return nil, err
+		}
+
+		data, err := decrypt.Data(content, "dotenv")
 		if err != nil {
 			return nil, err
 		}
--- sopsfiles/main.go
+++ sopsfiles/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"go.mozilla.org/sops/cmd/sops/common"
 	"go.mozilla.org/sops/decrypt"
 	"sigs.k8s.io/kustomize/k8sdeps/kv"
+	"sigs.k8s.io/kustomize/pkg/fs"
+	"sigs.k8s.io/kustomize/pkg/loader"
 )
 
 type sopsFiles struct{}
@@ -12,17 +13,29 @@ type sopsFiles struct{}
 var KVSource sopsFiles
 
 func (p *sopsFiles) Get(root string, args []string) (map[string]string, error) {
+	ldr, err := loader.NewLoader(root, fs.MakeRealFS())
+	if err != nil {
+		return nil, err
+	}
+
 	kvs := make(map[string]string)
 	for _, s := range args {
 		k, fPath, err := kv.ParseFileSource(s)
 		if err != nil {
 			return nil, err
 		}
-		format := formatForPath(fPath)
-		data, err := decrypt.File(fmt.Sprintf("%s/%s", root, fPath), format)
+
+		content, err := ldr.Load(fPath)
 		if err != nil {
 			return nil, err
 		}
+
+		format := formatForPath(fPath)
+		data, err := decrypt.Data(content, format)
+		if err != nil {
+			return nil, err
+		}
+
 		kvs[k] = string(data)
 	}
 	return kvs, nil
